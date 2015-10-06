308f6e2a8ec2fbc73d36b0861cac622f64d984e3
Teodoro Hochfarber Salinas <teohoch2@gmail.com>
2015-10-06 13:14:06 -0300

    Made the code parallel

--- sla_retriever.py
+++ sla_retriever.py
@@ -2,11 +2,38 @@ __author__ = 'teohoch'
 from astroquery.splatalogue import Splatalogue
 from astropy import units as u
 import time
+import threading
 
+
+class SlaGetter():
+    del Splatalogue.data["no_atmospheric"]
+    del Splatalogue.data["no_potential"]
+    del Splatalogue.data["no_probable"]
+
+    def getLines(self,lower, upper, location):
+        print("Downloading From "+ str(lower) + " to " + str(upper))
+        lines = Splatalogue.query_lines_async(
+            lower,
+            upper,
+            export=True,
+            export_limit=999999999,
+            line_strengths=('ls1', 'ls2', 'ls3', 'ls4', 'ls5'),
+            displayHFS=True,
+            show_unres_qn=True,
+            show_upper_degeneracy=True,
+            show_molecule_tag=True,
+            show_qn_code=True,
+            show_lovas_labref=True,
+            show_lovas_obsref=True,
+            show_orderedfreq_only=None,
+            show_nrao_recommended=True
+        )
+        print("Downloadg From " + str(lower) + " to " + str(upper) + " Completed | ") + lines.content.split("\n")[1]
+        Container[location] = lines.content
+
+        return lines
 #AStroquery has a bug that doesn't allow for setting this keys to anything, so everytime you want to query, it will exclude atmospheric, probable, and potential lines. To fix this, the keys in data must be removed
-del Splatalogue.data["no_atmospheric"]
-del Splatalogue.data["no_potential"]
-del Splatalogue.data["no_probable"]
+
 
 '''
 for i in range(47, 100000):
@@ -33,79 +60,27 @@ for i in range(47, 100000):
     t1 = time.time()
     print "Frequency from 0 to " + str(step) +"GHz, with " + str(len(lines.content.split("\n") )) + " lines in " + str(t1-t0)
     del lines
-'''
-print("Descargando 1° Segmento")
-lines = Splatalogue.query_lines_async(
-    0 * u.GHz,
-    470 * u.GHz,
-    export=True,
-    export_limit=999999999,
-    line_strengths=('ls1','ls2', 'ls3', 'ls4', 'ls5'),
-    displayHFS=True,
-    show_unres_qn=True,
-    show_upper_degeneracy=True,
-    show_molecule_tag=True,
-    show_qn_code=True,
-    show_lovas_labref=True,
-    show_lovas_obsref=True,
-    show_orderedfreq_only=None,
-    show_nrao_recommended=True
-    )
-print("Descargando 2° Segmento")
-lines2 = Splatalogue.query_lines_async(
-    470 * u.GHz,
-    1000 * u.GHz,
-    export=True,
-    export_limit=999999999,
-    line_strengths=('ls1','ls2', 'ls3', 'ls4', 'ls5'),
-    displayHFS=True,
-    show_unres_qn=True,
-    show_upper_degeneracy=True,
-    show_molecule_tag=True,
-    show_qn_code=True,
-    show_lovas_labref=True,
-    show_lovas_obsref=True,
-    show_orderedfreq_only=None,
-    show_nrao_recommended=True
-    )
-print("Descargando 3° Segmento")
-lines3 = Splatalogue.query_lines_async(
-    1000 * u.GHz,
-    1500 * u.GHz,
-    export=True,
-    export_limit=999999999,
-    line_strengths=('ls1','ls2', 'ls3', 'ls4', 'ls5'),
-    displayHFS=True,
-    show_unres_qn=True,
-    show_upper_degeneracy=True,
-    show_molecule_tag=True,
-    show_qn_code=True,
-    show_lovas_labref=True,
-    show_lovas_obsref=True,
-    show_orderedfreq_only=None,
-    show_nrao_recommended=True
-    )
-print("Descargando 4° Segmento")
-lines4 = Splatalogue.query_lines_async(
-    1500 * u.GHz,
-    100000 * u.THz,
-    export=True,
-    export_limit=999999999,
-    line_strengths=('ls1','ls2', 'ls3', 'ls4', 'ls5'),
-    displayHFS=True,
-    show_unres_qn=True,
-    show_upper_degeneracy=True,
-    show_molecule_tag=True,
-    show_qn_code=True,
-    show_lovas_labref=True,
-    show_lovas_obsref=True,
-    show_orderedfreq_only=None,
-    show_nrao_recommended=True
-    )
 
-with open("complete", "w") as f:
-    f.write(lines)
-    f.write(lines2)
-    f.write(lines3)
-    f.write(lines4)
+'''
+Container = ["","","",""]
+g = SlaGetter()
+t1 = threading.Thread(target=g.getLines,args=(0 * u.GHz,470 * u.GHz,0))
+t2 = threading.Thread(target=g.getLines,args=(470 * u.GHz,1000 * u.GHz,1))
+t3 = threading.Thread(target=g.getLines,args=(1000 * u.GHz,1500 * u.GHz,2))
+t4 = threading.Thread(target=g.getLines,args=(1500 * u.GHz,1000000* u.THz,3))
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+
+with open("complete_parallel", "w") as f:
+    for i in Container:
+        f.write(i)
+
 
