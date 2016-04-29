20864fed1b5d98ac75f4f8a2fa62ae03c5935213
Teodoro Hochfarber Salinas <teohoch2@gmail.com>
2016-04-29 13:38:36 -0300

    transformed Query into a class method, so it can be used without instantiation. Also now query fields uses a regex to find the fields. If the regex is further refined should save a lot of code

--- SlapClient.py
+++ SlapClient.py
@@ -1,5 +1,6 @@
 import requests
 from astropy.io import votable
+import re
 from astropy.io.votable.tree import VOTableFile, Resource, Table, Field, Info
 
 import StringIO
@@ -7,76 +8,80 @@ import StringIO
 __author__ = "teohoch"
 
 
-class SlapClient():
-	def __init__(self, slap_service, slap_version=1.0):
-		"""
+class SlapClient(object):
+    def __init__(self, slap_service, slap_version=1.0):
+        """
 
-		:type slap_version: Double
-		:type slap_service: String
+        :type slap_version: Double
+        :type slap_service: String
 
-		"""
-		self.__slap_service = slap_service
-		self.__slap_version = slap_version
+        """
+        self.__slap_service = slap_service
+        self.__slap_version = slap_version
 
-	def query(self, **kwargs):
-		query_params = self.__parser(kwargs)
+    @classmethod
+    def query(cls, service, slap_version=1.0, **kwargs):
+        query_params = SlapClient.__parser(slap_version, kwargs)
+        if query_params:
+            request = requests.get(service, params=query_params)
+            return request.text
+        else:
+            raise ValueError("The query is not valid according to the SLA Protocol")
 
-		if query_params:
-			request = requests.get(self.__slap_service, params=query_params)
-			return request.text
+    @classmethod
+    def __parser(cls, slap_version, parameters):
 
-		else:
-			raise ValueError("The query is not valid according to the SLA Protocol")
+        query_params = {"VERSION": slap_version,
+                        "REQUEST": "queryData"}
 
-	def query_votable(self, **kwargs):
-		result = self.query(kwargs)
-		temp = StringIO.StringIO()
-		temp.write(result.text)
-		return votable.parse(temp)
+        for constrain_name, constrain in parameters.iteritems():
 
-	def query_fields(self):
-		basic = self.query(WAVELENGTH=1.0)  # Splatalogue can't deal with wavelenght == 0 XD!
-		temp = StringIO.StringIO()
-		temp.write(basic)
-		data = votable.parse(temp)
-		print data.params
-		result = []
-		for field in data.params:
-			result.append(field)
-		return result
+            if constrain_name.lower() == "wavelength":
+                valid = True
 
+            if isinstance(constrain, dict):  # range
+                pass
+            elif isinstance(constrain, list):  # list of constrains
+                pass
+            else:  # equality
+                query_params[constrain_name.upper()] = cls.__equality(constrain)
+        return query_params if valid else None
 
+    def query_service(self,**kwargs):
+        return self.query(self.__slap_service, self.__slap_version, kwargs)
 
-	def __parser(self, parameters):
+    def query_votable(self, **kwargs):
+        result = self.query(kwargs)
+        temp = StringIO.StringIO()
+        temp.write(result.text)
+        return votable.parse(temp)
 
-		query_params = {"VERSION": self.__slap_version,
-						"REQUEST": "queryData"}
+    def query_fields(self):
+        basic = SlapClient.query(self.__slap_service,self.__slap_version, wavelength=1.0)  # Splatalogue can't deal with wavelenght == 0 XD!
+        # Use Regex <FIELD (.*?)>.*?<DESCRIPTION>(.*?)<\/DESCRIPTION>.*?<\/FIELD>
+        # https://regex101.com/r/pL7pY1/1
+        p = re.compile(ur'<FIELD (.*?)>.*?<DESCRIPTION>(.*?)<\/DESCRIPTION>.*?<\/FIELD>', re.UNICODE | re.DOTALL)
+        subst = u""
 
-		for constrain_name, constrain in parameters.iteritems():
+        result = re.findall(p, basic)
 
-			if constrain_name.lower() == "wavelength":
-				valid = True
+        return result
 
-			if isinstance(constrain, dict):  # range
-				pass
-			elif isinstance(constrain, list):  # list of constrains
-				pass
-			else:  # equality
-				query_params[constrain_name.upper()] = self.__equality(constrain)
-		return query_params if valid else None
+    @staticmethod
+    def __range(params):
+        pass
 
-	def __range(self, params):
-		pass
+    @staticmethod
+    def __equality(params):
+        return params
 
-	def __equality(self, params):
-		return params
-
-	def __list(self):
-		pass
+    @staticmethod
+    def __list(params):
+        pass
 
 
 if __name__ == "__main__":
-	service = "https://find.nrao.edu/splata-slap/slap"
-	client = SlapClient(service)
-	data = client.query_fields()
-	print data
+    service = "https://find.nrao.edu/splata-slap/slap"
+    client = SlapClient(service)
+    data = client.query_fields()
+    print data
